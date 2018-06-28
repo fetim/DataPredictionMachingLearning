fec3ba3a3a4ea682ea3576a68d18d2e9439b8a92
fetim <felipetimoteo@id.uff.br>
2018-06-28 12:54:24 -0300

    Plot working

--- Importdata.py
+++ Importdata.py
@@ -9,12 +9,17 @@ import numpy as np
 import matplotlib.pylab as pl
 
 filename="WLC_PETRO_COMPUTED_INPUT_1_SEMHEADER.las"
+
 Matrix = np.loadtxt(filename, unpack = True)
 
 np.shape(Matrix)
 
+Matrix[ Matrix==-999.25] = np.nan
 depth = Matrix[0,:]
-log1 = Matrix[1,:]
+log1 = Matrix[7,:]
+# log2 =Matrix[8,:]
+# log3 =Matrix[14,:]
+
 
 print(np.shape(log1))
 print(np.shape(depth))
